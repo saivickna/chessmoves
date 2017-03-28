66d79cc9051597fd082df125b119d9e000095b28
Saivickna Raveendran <saivickna.r@gmail.com>
2017-03-28 14:17:54 -0700

    Remove functionality that allows for duplicate pieces

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ var whitePieces = {};
 var blackPieces = {};
 
 //Originally planned on limiting placement of pieces but realized promotion can allow multiple of the same piece
-var availWhite = {'King':1, 'Queen':1, 'Bishop':2, 'Rook':2, 'Knight':2, 'Pawn':8 };
-var availBlack = {'King':1, 'Queen':1, 'Bishop':2, 'Rook':2, 'Knight':2, 'Pawn':8 };
+// var availWhite = {'King':1, 'Queen':1, 'Bishop':2, 'Rook':2, 'Knight':2, 'Pawn':8 };
+// var availBlack = {'King':1, 'Queen':1, 'Bishop':2, 'Rook':2, 'Knight':2, 'Pawn':8 };
 var moveFunctions = {'King': move.king, 'Queen': move.queen, 'Bishop': move.bishop, 'Rook': move.rook, 'Knight': move.knight};
 
 
@@ -19,13 +19,8 @@ for (var i = 7; i >= 0; i--) {
     colDiv.id = cols[j] + i;
     colDiv.innerText = cols[j] + i;
     colDiv.addEventListener('click', e => {
-      if (e.target.classList.contains('black')) {
-        e.target.classList.remove('black');
-        availBlack[e.target.innerText]++;
-      } else if (e.target.classList.contains('white')) {
-        e.target.classList.remove('white');
-        availWhite[e.target.innerText]++;
-      }
+      e.target.classList.remove('black');
+      e.target.classList.remove('white');
       delete whitePieces[e.target.id];
       delete blackPieces[e.target.id];
       e.target.innerText = e.target.id;
@@ -45,17 +40,13 @@ var addPiece = function(x, y) {
   let cell = document.getElementById(col+row);
   if (cell.innerText !== cell.id) {
     alert('Cell is occupied. Please click on the cell to delete before assigning another piece');
-  } else if ((player === 'white' && availWhite[piece] === 0) || (player === 'black' && availBlack[piece] === 0) ) {
-    alert('No available pieces of that type left.');
   } else {
     cell.classList.add(player);
     cell.innerText = piece;
     if (player === 'white') {
       whitePieces[(col+row)] = piece;
-      availWhite[piece]--;
     } else {
       blackPieces[(col+row)] = piece;
-      availBlack[piece]--;
     }
   }
 }
