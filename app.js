1ce4cd0f0e8f00bd58ead4b0de0ca86533063a64
Saivickna Raveendran <saivickna.r@gmail.com>
2017-03-28 14:50:24 -0700

    Add comments and update logic to handle row values of 1-8

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var moveFunctions = {'King': move.king, 'Queen': move.queen, 'Bishop': move.bish
 
 //Initialize board
 var board = document.getElementById('board')
-for (var i = 7; i >= 0; i--) {
+for (var i = 8; i >= 1; i--) {
   var rowDiv = document.createElement('div');
   rowDiv.className = 'column'
   for (var j = 0; j < 8; j++) {
@@ -18,11 +18,12 @@ for (var i = 7; i >= 0; i--) {
     colDiv.className = 'row';
     colDiv.id = cols[j] + i;
     colDiv.innerText = cols[j] + i;
+    //add event listeners to each item on the board to reset it on click
     colDiv.addEventListener('click', e => {
       e.target.classList.remove('black');
       e.target.classList.remove('white');
-      delete whitePieces[e.target.id];
-      delete blackPieces[e.target.id];
+      delete whitePieces[e.target.id[0] + (e.target.id[1]-1)];
+      delete blackPieces[e.target.id[0] + (e.target.id[1]-1)];
       e.target.innerText = e.target.id;
     })
     rowDiv.appendChild(colDiv);
@@ -30,7 +31,11 @@ for (var i = 7; i >= 0; i--) {
   board.appendChild(rowDiv);
 }
 
-var addPiece = function(x, y) {
+
+
+
+//addPiece function takes the inputs of the drop downs on the board, updates the UI, and updates the list of pieces added in memory
+var addPiece = function() {
   let col = document.getElementById('columnSelect').value;
   let row = document.getElementById('rowSelect').value;
   let piece = document.getElementById('pieceSelect').value;
@@ -38,44 +43,57 @@ var addPiece = function(x, y) {
   let player = players[0].checked ? players[0].value : players[1].value;
 
   let cell = document.getElementById(col+row);
+  //Prevent updating a cell if it is already occupied
   if (cell.innerText !== cell.id) {
     alert('Cell is occupied. Please click on the cell to delete before assigning another piece');
   } else {
     cell.classList.add(player);
     cell.innerText = piece;
     if (player === 'white') {
-      whitePieces[(col+row)] = piece;
+      whitePieces[(col+(row-1))] = piece;
     } else {
-      blackPieces[(col+row)] = piece;
+      blackPieces[(col+(row-1))] = piece;
     }
   }
 }
 
-var addBtn = document.getElementById('add');
-addBtn.addEventListener('click', addPiece);
-var moveList = document.getElementById('moveList');
-var calculateMoveBtn = document.getElementById('calculate');
-calculateMoveBtn.addEventListener('click', e => {
- let colors = document.getElementsByName('calculatePlayer');
- let color = colors[0].checked ? colors[0].value : colors[1].value;
- let moves = [];
- if (color === 'white') {
-  for (var piece in whitePieces) {
-    moves = moves.concat(moveFunctions[whitePieces[piece]] ? moveFunctions[whitePieces[piece]](piece.charCodeAt(0) - 97, parseInt(piece[1]), whitePieces, blackPieces) : 
+
+//calculate move list takes the current list of pieces on the board and who's turn it is to calculate the position of all pieces on the board
+var calculateMoveList = function() {
+
+  //determine which player is going
+  let colors = document.getElementsByName('calculatePlayer');
+  let color = colors[0].checked ? colors[0].value : colors[1].value;
+  let moves = [];
+
+  //The pawn function has an additional parameter and must operate different as pawns can only move in one direction
+  //The remaining pieces are iterated over and the representative functions are called
+  if (color === 'white') {
+    for (var piece in whitePieces) {
+      moves = moves.concat(moveFunctions[whitePieces[piece]] ? moveFunctions[whitePieces[piece]](piece.charCodeAt(0) - 97, parseInt(piece[1]), whitePieces, blackPieces) : 
                           move.pawn(piece.charCodeAt(0) - 97, parseInt(piece[1]), whitePieces, blackPieces, true));
-  }
- } else {
-  for (var piece in blackPieces) {
-    moves = moves.concat(moveFunctions[blackPieces[piece]] ? moveFunctions[blackPieces[piece]](piece.charCodeAt(0) - 97, parseInt(piece[1]), blackPieces, whitePieces) : 
+    }
+  } else {
+    for (var piece in blackPieces) {
+      moves = moves.concat(moveFunctions[blackPieces[piece]] ? moveFunctions[blackPieces[piece]](piece.charCodeAt(0) - 97, parseInt(piece[1]), blackPieces, whitePieces) : 
                           move.pawn(piece.charCodeAt(0) - 97, parseInt(piece[1]), whitePieces, blackPieces, false));
+    }
   }
- }
+
+  //Remove any previous moves from the move list and update the UI
+  let moveList = document.getElementById('moveList');
   while (moveList.firstChild) {
-      moveList.removeChild(moveList.firstChild);
+    moveList.removeChild(moveList.firstChild);
   }
- for (var i = 0; i < moves.length; i++) {
+  //The move calculation is decoupled from the UI, so from the calculation perspective the data goes from 0-7 but on the UI it goes from 1-8
+  //String manipulation must be done to handle this
+  for (var i = 0; i < moves.length; i++) {
     var moveListItem = document.createElement('li');
-    moveListItem.innerText = moves[i];
+    moveListItem.innerText = moves[i].substring(0, moves[i].length - 1) + (parseInt(moves[i][moves[i].length-1]) + 1);
     moveList.appendChild(moveListItem);
- }
-});
+  }
+}
+
+//Add event handler functions for buttons
+document.getElementById('add').addEventListener('click', addPiece);
+document.getElementById('calculate').addEventListener('click', calculateMoveList);
