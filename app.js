372aa7e24925ee9c8b4b0c2b795d29978ac350d6
Saivickna Raveendran <saivickna.r@gmail.com>
2017-03-28 14:14:34 -0700

    Add calculation functionality into app

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
+//Initialize pieces and calls to pieces
 var whitePieces = {};
 var blackPieces = {};
+
+//Originally planned on limiting placement of pieces but realized promotion can allow multiple of the same piece
 var availWhite = {'King':1, 'Queen':1, 'Bishop':2, 'Rook':2, 'Knight':2, 'Pawn':8 };
 var availBlack = {'King':1, 'Queen':1, 'Bishop':2, 'Rook':2, 'Knight':2, 'Pawn':8 };
-var moveFunctions = {'King': move.king, 'Queen': move.queen, 'Bishop': move.bishop, 'Rook': move.rook, 'Knight': move.knight, 'Pawn': move.pawn};
+var moveFunctions = {'King': move.king, 'Queen': move.queen, 'Bishop': move.bishop, 'Rook': move.rook, 'Knight': move.knight};
+
+
 //Initialize board
-var iDiv = document.createElement('div');
-iDiv.className = 'block';
 var board = document.getElementById('board')
-board.appendChild(iDiv);
 for (var i = 7; i >= 0; i--) {
   var rowDiv = document.createElement('div');
   rowDiv.className = 'column'
@@ -33,9 +35,7 @@ for (var i = 7; i >= 0; i--) {
   board.appendChild(rowDiv);
 }
 
-
-var addBtn = document.getElementById('add');
-addBtn.addEventListener('click', e => {
+var addPiece = function(x, y) {
   let col = document.getElementById('columnSelect').value;
   let row = document.getElementById('rowSelect').value;
   let piece = document.getElementById('pieceSelect').value;
@@ -58,4 +58,33 @@ addBtn.addEventListener('click', e => {
       availBlack[piece]--;
     }
   }
+}
+
+var addBtn = document.getElementById('add');
+addBtn.addEventListener('click', addPiece);
+var moveList = document.getElementById('moveList');
+var calculateMoveBtn = document.getElementById('calculate');
+calculateMoveBtn.addEventListener('click', e => {
+ let colors = document.getElementsByName('calculatePlayer');
+ let color = colors[0].checked ? colors[0].value : colors[1].value;
+ let moves = [];
+ if (color === 'white') {
+  for (var piece in whitePieces) {
+    moves = moves.concat(moveFunctions[whitePieces[piece]] ? moveFunctions[whitePieces[piece]](piece.charCodeAt(0) - 97, parseInt(piece[1]), whitePieces, blackPieces) : 
+                          move.pawn(piece.charCodeAt(0) - 97, parseInt(piece[1]), whitePieces, blackPieces, true));
+  }
+ } else {
+  for (var piece in blackPieces) {
+    moves = moves.concat(moveFunctions[blackPieces[piece]] ? moveFunctions[blackPieces[piece]](piece.charCodeAt(0) - 97, parseInt(piece[1]), blackPieces, whitePieces) : 
+                          move.pawn(piece.charCodeAt(0) - 97, parseInt(piece[1]), whitePieces, blackPieces, false));
+  }
+ }
+  while (moveList.firstChild) {
+      moveList.removeChild(moveList.firstChild);
+  }
+ for (var i = 0; i < moves.length; i++) {
+    var moveListItem = document.createElement('li');
+    moveListItem.innerText = moves[i];
+    moveList.appendChild(moveListItem);
+ }
 });
